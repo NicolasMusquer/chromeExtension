90ed5ca5abd718ef92ba89618c47af13a18148c4
Nicolas Musquer <nicolas.m@agence-pm.com>
2020-12-16 14:37:45 +0100

    first

--- src/scripts/linkedin.ts
+++ src/scripts/linkedin.ts
@@ -1,10 +1,29 @@
-import { Result } from "../models/result";
+import { Profile } from "../models/profile";
 
-chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
-  const result: Result = {
-    message: "Hello"
+async function getProfileInfo(): Promise<Profile> {
+  const fullName = document.querySelectorAll(".pv-top-card--list li")[0];
+  const title = document.querySelectorAll(".pv-top-card h2.mt1")[0];
+
+  const response: Profile = {
+    fullName: fullName.textContent.replace(/\n/, "").trim(),
+    title: title.textContent.replace(/\n/, "").trim(),
+    country: "XXX"
   };
 
-  response(result.message);
+  return response;
+}
+
+let profile: Profile = null;
+setTimeout(() => {
+  getProfileInfo().then(result => {
+    profile = result || profile;
+  });
+}, 3000);
+
+chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
+  if (msg.from === "popup" && msg.subject === "getFullName") {
+    response(profile);
+  }
+
   return true;
 });
